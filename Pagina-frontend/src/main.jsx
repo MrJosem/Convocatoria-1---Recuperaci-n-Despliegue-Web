3c7fe1ce5aeba426e633eeed04dbddf5b1684c46
JoseManuel134975 <josemanuel3606@gmail.com>
2025-10-16 09:35:39 +0200

    Página con frontend subida + nombres y descripciones cambiados en .ymls + .gitignore

---
+++ Pagina-frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+import './myIndex.css'
+import MyApp from './MyApp.jsx'
+
+/**
+ * 
+ */
+
+createRoot(document.getElementById('root')).render(
+    <MyApp />
+  ,
+)
